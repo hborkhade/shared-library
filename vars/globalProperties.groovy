cb745f1d40f0bd8a4c07e00761afaa5e410dd7ed
hborkhade <32675232+hborkhade@users.noreply.github.com>
2017-10-11 18:03:22 +0530

    Update globalProperties.groovy

--- vars/globalProperties.groovy
+++ vars/globalProperties.groovy
@@ -2,9 +2,11 @@ class Load implements Serializable {
 
   def projectName
   def serverDomain
+  def dynamicProperty
 
   Load() {
     this.projectName = "Project2"
     this.serverDomain = "Project2 server domain"
+    this.dynamicProperty = "%MSG% this is Dynamic property"
   }
 }
