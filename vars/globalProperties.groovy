14b29acdd1807419267a30bf9df1c9fd48169f32
hborkhade <32675232+hborkhade@users.noreply.github.com>
2017-10-11 13:54:02 +0530

    Create globalProperties.groovy

---
+++ vars/globalProperties.groovy
@@ -0,0 +1,10 @@
+class Load implements Serializable {
+
+  def projectName
+  def serverDomain
+
+  Load() {
+    this.projectName = "Project2"
+    this.serverDomain = "Project2 server domain"
+  }
+}
